8d62a743271d01d6fe6d796d2155f58136b4d3a4
rifqilubis <rifqi.raihan.lubis@gmail.com>
2025-05-20 10:16:27 +0700

    udh hampir chapter 3

---
+++ player.py
@@ -0,0 +1,37 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        # Call the parent constructor with the player radius
+        super().__init__(x, y, PLAYER_RADIUS)
+        # Initialize rotation to 0
+        self.rotation = 0
+        
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        # Override the draw method to draw a triangle instead of a circle
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # Rotate left (negative dt)
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            # Rotate right (positive dt)
+            self.rotate(dt)
+
+            
+    def rotate(self, dt):
+        # Add rotation based on turn speed and time elapsed
+        self.rotation += PLAYER_TURN_SPEED * dt
